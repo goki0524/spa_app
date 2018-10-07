c6ee0ca1fb2c956a08842a9d7eb71b5bd92be2dd
goki sakaguchi <goki0524@gmail.com>
2018-10-07 15:42:13 +0900

    fix TaskController

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Task;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        return Task::take(5)->get()->keyBy('id');
+    }
+
+    public function store(Request $request)
+    {
+       return Task::create($request->only('name'))->fresh();
+        
+    }
+
+    public function destroy($id)
+    {
+        return Task::destroy($id);
+    }
+
+    public function update($id, Request $request)
+    { 
+        $task = Task::find($id)->fill($request->only('is_done'));
+        $task->save();
+        return $task->fresh();
+    }
+}
