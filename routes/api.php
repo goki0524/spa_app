f1ef8dcea225d3c7ae908f0ca86a96e1dc0dfeca
goki sakaguchi <goki0524@gmail.com>
2018-10-18 21:11:56 +0900

    Add AuthneticateController

--- routes/api.php
+++ routes/api.php
@@ -14,5 +14,10 @@ use Illuminate\Http\Request;
 */
 
 Route::group(['middleware' => 'api'], function () {
-    Route::resource('tasks',  'TaskController');
+    Route::post('authenticate', 'AuthenticateController@authenticate');
+
+    Route::group(['middlerare' => 'jwt.auth'], function(){
+        Route::resource('tasks',  'TaskController');
+        Route::get('me', 'AuthenticateController@getCurrentUser');
+    });
 });
